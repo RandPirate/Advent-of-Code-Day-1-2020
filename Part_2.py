e42af0a8fa316cacdb196f6e283dc35508784a0d
RandPirate <74821524+RandPirate@users.noreply.github.com>
2020-12-22 14:17:23 +0100

    Update Part_2.py

--- Part_2.py
+++ Part_2.py
@@ -1,5 +1,5 @@
 from itertools import combinations
-# import numpy as np
+
 
 open = open("input.txt", "r")
 content = open.read()
@@ -9,21 +9,8 @@ del splitlist[200]
 
 final = list(map(int, splitlist))
 
-
-
-# for i in range(len(final)):
-#     for c in combinations(final, i):
-#         combination = c
-
 combs = combinations(final, 3)
 
 for c in combs:
     if sum(c) == 2020:
         print(c)
-
-
-# sol = [c for c in combs if sum(c) == 2020]
-
-# print(sol)
-
-# np.prod(sol)
